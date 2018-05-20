d92dec74209a74c023646401c360ece04b1485ee
Himanshu Negi <ecoworld007@gmail.com>
2018-05-21 03:50:11 +0530

    using try catch block to throw error

--- currency-convert.js
+++ currency-convert.js
@@ -10,10 +10,17 @@ const getExchangeRate = (from, to) => {
 getExchangeRate('USD', 'CAD').then(rate => console.log(rate));
 
 const getExchangeRateAlt = async (from, to) => {
-    let response = await axios.get('http://data.fixer.io/api/latest?access_key=8ad0ca89f449766f64095f2c9e92d04c&format=1');
-    let euro = 1 / response.data.rates[from];
-    let rate = euro * response.data.rates[to];
-    return rate;
+    try{
+        let response = await axios.get('http://data.fixer.io/api/latest?access_key=8ad0ca89f449766f64095f2c9e92d04c&format=1');
+        let euro = 1 / response.data.rates[from];
+        let rate = euro * response.data.rates[to];
+        if(isNaN(rate)){
+            throw new Error();
+        }
+        return rate;
+    }catch(err){
+        throw new Error(`unable to get exchange rate for ${from} to ${to}`);
+    } 
 }
 getExchangeRateAlt('USD', 'INR').then(rate => console.log(rate));
 
@@ -25,8 +32,13 @@ const getCountries = (currency) => {
 getCountries('USD').then(countries => console.log(countries));
 
 const getCountriesAlt = async (currency) => {
-    let response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currency}`);
-    return response.data.map(country => country.name);
+    try{
+        let response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currency}`);
+        return response.data.map(country => country.name);
+    }catch(err){
+        throw new Error(`unable to get countries which uses: ${currency}`);
+    }
+    
 };
 getCountriesAlt('INR').then(countries => console.log(countries));
 
@@ -37,4 +49,4 @@ const convertCurrency = async (from, to, amount) => {
     return `${amount}  ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries.join(', ')}`;
 };
 
-convertCurrency('USD', 'INR', 120000).then(message => console.log(message));
+convertCurrency('USD', 'INR', 120000).then(message => console.log(message)).catch(err => console.log(err));
