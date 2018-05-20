b4ae4641324de017f441d064f0c1803fa5e1dada
Himanshu Negi <ecoworld007@gmail.com>
2018-05-21 03:35:56 +0530

    gitignored nodemodule

---
+++ currency-convert.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+const getExchangeRate = (from, to) => {
+    return axios.get('http://data.fixer.io/api/latest?access_key=8ad0ca89f449766f64095f2c9e92d04c&format=1').then(response => {
+        let euro = 1 / response.data.rates[from];
+        let rate = euro * response.data.rates[to];
+        return rate;
+    })
+};
+getExchangeRate('USD', 'CAD').then(rate => console.log(rate));
+
+const getExchangeRateAlt = async (from, to) => {
+    let response = await axios.get('http://data.fixer.io/api/latest?access_key=8ad0ca89f449766f64095f2c9e92d04c&format=1');
+    let euro = 1 / response.data.rates[from];
+    let rate = euro * response.data.rates[to];
+    return rate;
+}
+getExchangeRateAlt('USD', 'INR').then(rate => console.log(rate));
+
+const getCountries = (currency) => {
+    return axios.get(`https://restcountries.eu/rest/v2/currency/${currency}`).then(response => {
+        return response.data.map(country => country.name);
+    })
+};
+getCountries('USD').then(countries => console.log(countries));
+
+const getCountriesAlt = async (currency) => {
+    let response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currency}`);
+    return response.data.map(country => country.name);
+};
+getCountriesAlt('INR').then(countries => console.log(countries));
+
+const convertCurrency = async (from, to, amount) => {
+    let rate = await getExchangeRate(from, to);
+    let convertedAmount = (rate * amount).toFixed(2);
+    let countries = await getCountries(to);
+    return `${amount}  ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries.join(', ')}`;
+};
+
+convertCurrency('USD', 'INR', 120000).then(message => console.log(message));
